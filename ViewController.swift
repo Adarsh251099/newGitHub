ee3e3392ee40f92e87d245009d78f1424f9a8499
Adarsh251099 <adarshmandale12@gmail.com>
2022-09-19 04:00:13 -0700

    first init

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newGitHub
+//
+//  Created by Capgemini-DA184 on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
